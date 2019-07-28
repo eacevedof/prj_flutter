7522c4a05db509ec0570327f69a0566e803dc1f3
Eduardo A.F <eacevedof@yahoo.es>
2019-07-28 21:32:30 +0200

    7.4 Inicio del proyecto - Películas

---
+++ flutter_peliculas/README.md
@@ -0,0 +1,5 @@
+# CAP 7 - flutter_peliculas
+### [Código fuente original]()
+
+- 7.4 Inicio del proyecto - Películas
+    - 
---
+++ flutter_peliculas/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_peliculas/src/pages/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      title: 'Peliculas',
+      initialRoute: "/",
+      routes: {
+        "/" : (BuildContext context) => HomePage(),
+      },
+    );
+
+  }// build
+
+}// MyApp
