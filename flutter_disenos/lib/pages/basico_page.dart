bbed8d04e6f8192e35db1b18badda4c4f1181633
Eduardo A.F <eacevedof@yahoo.es>
2019-08-09 21:14:54 +0200

    8.6 6. Diseño básico - parte 1

--- flutter_disenos/lib/pages/basico_page.dart
+++ flutter_disenos/lib/pages/basico_page.dart
@@ -6,8 +6,13 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Basico Page"),
+      body: Column(
+        children: <Widget>[
+          Container(child: Text("img1"),),
+          Container(child: Text("texto estrella"),),
+          Container(child: Text("iconos telefono y share"),),
+          Container(child: Text("texto amplio"),)
+        ]
       ),
     );
   }// build
