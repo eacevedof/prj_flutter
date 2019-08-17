a8a3970690a1781364ef899c2ca364925e2f0afe
Eduardo A.F <eacevedof@yahoo.es>
2019-08-17 18:45:37 +0200

    11.5. Diseño del fondo de pantalla del login

--- flutter_formvalidation/lib/src/pages/login_page.dart
+++ flutter_formvalidation/lib/src/pages/login_page.dart
@@ -6,10 +6,63 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Login Page"),
+      //stack: div que permite colapsar elementos
+      body: Stack(
+        children: <Widget>[
+          _get_fondo_wg(context),
+        ],
       ),
     );
   }//build
+
+  Widget _get_fondo_wg(BuildContext context){
+    final size = MediaQuery.of(context).size;
+
+    final fondomorado = Container(
+      height: size.height * 0.4,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: <Color> [
+            Color.fromRGBO(63, 63, 15, 1.0),
+            Color.fromRGBO(90, 70, 178, 1.0),
+          ]
+        )
+      ),
+    );
+  
+    final circulo = Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(100.0),
+        color: Color.fromRGBO(255, 255, 255, 0.05)
+      ),
+    );
+
+    return Stack(
+      children: <Widget>[
+        fondomorado,
+        //permite aplicar una coordenada de posicion
+        Positioned(top:90.0,left: 30.0,child:circulo),
+        Positioned(top:-40.0,right: -30.0,child:circulo),
+        Positioned(bottom:-50.0,right: -10.0,child:circulo),
+        Positioned(bottom:120.0,right: -20.0,child:circulo),
+        Positioned(bottom:-50.0,left: -20.0,child:circulo),
+        
+        Container(
+          padding: EdgeInsets.only(top: 80.0),
+          child: Column(
+            children: <Widget>[
+              Icon(Icons.person_pin_circle, color:Colors.white, size:100.0),
+              SizedBox(height: 10.0, width: double.infinity,),
+              Text("Fernando Herrera", style: TextStyle(color: Colors.white, fontSize: 25.0),)
+            ],
+          ),
+        ),
+      ],
+    );
+
+  }//_get_fondo_wg 
 
 }//class LoginPage
